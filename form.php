2fbddee426eee5ab3deb9950a1bb458ebc3079c4
Lisandro Borda <@lichiborda@gmail.com>
2023-03-09 19:25:52 -0300

    primer repositorio

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['enviar'])) { 
+    if (!empty($_POST['name']) && !empty($_POST['msg']) && !
+    empty($_POST['email'])) {
+    $name = $_POST['name'];
+    $msg = $_POST['msg'];
+    $email = $_POST['email']; I
+    $header = "From: noreply@example.com" . "\r\n";
+    $header.= "Reply-To: noreply@example.com" . "\r\n";
+    $header.= "X-mailer: PHP/". phpversion();
+    $mail = mail($email,$msg,$header);
+   
+    }
+
+    }
+
+
+?>
